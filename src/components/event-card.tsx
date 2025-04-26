cf476320413f082efbad5e363f93c1995e60315e
curiouscoder00 <kapiljangid040@gmail.com>
2025-04-26 15:53:20 +0530

    feat: enhance EventCard component with isDragging prop for improved drag-and-drop experience; refactor styles and structure for better responsiveness

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,28 +1,24 @@
 "use client";
 
-import { EventCardProps } from "@/types";
 import React from "react";
-import { motion } from "motion/react";
-import useCalendar from "@/hooks/use-calendar";
+import { motion } from "framer-motion";
+import { EventCardProps } from "@/types";
 import Image from "next/image";
-const EventCard = ({ event }: EventCardProps) => {
-  const calendar = useCalendar();
 
-  const handleClick = () => {
-    calendar.setSelectedEvent(event);
-  };
+const EventCard = ({ event, isDragging = false }: EventCardProps) => {
   return (
     <motion.div
-      className={`bg-white rounded-lg shadow-md overflow-hidden cursor-grab hover:shadow-md hover:shadow-black/20 h-full`}
-      onClick={handleClick}
+      className={`bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-transform ${
+        isDragging ? "opacity-70 cursor-grabbing scale-50 shadow-sm shadow-blue-500" : ""
+      }`}
       whileHover={{ scale: 1.02 }}
-      whileTap={{ scale: 0.95 }}
+      whileTap={{ scale: 0.97 }}
       layoutId={`event-${event.id}`}
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.2 }}
     >
-      <div className="md:h-24 sm:h-44 h-24 w-full relative">
+      <div className="relative w-full h-24 md:h-24 sm:h-44">
         <Image
           src={event.imageUrl}
           alt={event.title}
@@ -30,12 +26,12 @@ const EventCard = ({ event }: EventCardProps) => {
           height={500}
           className="w-full h-full object-cover"
         />
-        <div className="absolute top-2 right-2 bg-gradient-active text-white text-xs font-medium px-2 py-1 rounded-full">
+        <div className="absolute top-2 right-2 bg-gradient-active text-white text-xs font-semibold px-2 py-1 rounded-full">
           {event.time}
         </div>
       </div>
       <div className="p-3">
-        <h3 className="font-medium text-gray-800 line-clamp-1">{event.title}</h3>
+        <h3 className="font-semibold text-gray-800 line-clamp-1">{event.title}</h3>
         <p className="text-sm text-gray-500 line-clamp-2 mt-1">
           {event.description}
         </p>
